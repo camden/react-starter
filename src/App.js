a86954cbb9f81d385b6ef798a7fdcf4e6230a391
Camden Bickel <cam@bickel.io>
2017-09-07 23:03:01 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+// @flow
+
+import React from 'react';
+
+const App = () => {
+  return <div>Hello world!</div>;
+};
+
+export default App;
---
+++ src/webpack.config.js
@@ -0,0 +1,37 @@
+var webpack = require('webpack');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devtool: 'cheap-module-source-map',
+  entry: ['./src/index.js'],
+  output: {
+    path: path.join(__dirname, '../dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    modules: ['node_modules'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: __dirname,
+        use: {
+          loader: 'babel-loader',
+        },
+      }
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'production'),
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+  ],
+};
+
